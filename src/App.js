b4ff0f4a4945840c21730ab51d856767e351c42a
Olha <gorokhovianka@gmail.com>
2021-12-25 23:35:23 +0200

    Corrected file Profile
    Delete file App.js from "src/components\App.js"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
 import FriendList from "./components/FriendList/FriendList";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-// ----------------------------------------------------------------------
+
 import user from "./data/user.json";
 import statisticalData from "./data/statistical-data.json";
 import friendsData from "./data/friends.json";
@@ -11,14 +11,7 @@ import transactions from "./data/transactions.json";
 const App = () => {
   return (
     <div>
-      <Profile
-        {...user}
-        // avatar={user.avatar}
-        // name={user.name}
-        // tag={user.tag}
-        // location={user.location}
-        // stats={user.stats}
-      />
+      <Profile {...user} />
       <Statistics title="Upload stats" stats={statisticalData} />
       <FriendList friends={friendsData} />
       <TransactionHistory items={transactions} />;
